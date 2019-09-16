0d58145ab8f2a79c761e6ff8e29cc705d9fc77f3
Advait Balaji <adsgarfield95@gmail.com>
2019-09-16 23:27:03 +0000

    Replace test.cpp
    Former-commit-id: 96b8242674cd1c12955225839b1abd55088860f5

--- test.cpp
+++ test.cpp
@@ -122,8 +122,8 @@ void  readSam(const string filename, BOOST_C::map<string,boost::unordered_set<in
 }
 
 //Create final set unitigs to connect
-BOOST_C::map<int,boost::unordered_set<int> > processDictionary( BOOST_C::map<string,boost::unordered_set<int> > &u_map1, BOOST_C::map<string,boost::unordered_set<int> > &u_map2){
-	BOOST_C::map<int,boost::unordered_set<int> > u_map;
+BOOST_C::map<int,BOOST_C::vector<int> > processDictionary( BOOST_C::map<string,boost::unordered_set<int> > &u_map1, BOOST_C::map<string,boost::unordered_set<int> > &u_map2){
+	BOOST_C::map<int,BOOST_C::vector<int> > u_map;
 	boost::unordered_set<customset::cset> seen_set;
 	BOOST_C::vector<string> pair_reads;
 	printConsole("Filter single pairs");
@@ -147,7 +147,8 @@ BOOST_C::map<int,boost::unordered_set<int> > processDictionary( BOOST_C::map<str
 			auto it_set = seen_set.find(temp_cset);
 			if(it_set==seen_set.end()){	
 				seen_set.insert(temp_cset);
-				u_map[count++] = temp_cset.uset;
+				BOOST_C::vector<int> temp(temp_cset.uset.begin(),temp_cset.uset.end());
+				u_map[count++] = temp;
 			}
 		}
 	}
@@ -160,37 +161,39 @@ BOOST_C::map<int,boost::unordered_set<int> > processDictionary( BOOST_C::map<str
 }
 
 
-void createGraph(BOOST_C::map<int,boost::unordered_set<int> > &u_map, int num_unitig){	
-	//set of seen pairs
+void createGraph(BOOST_C::map<int,BOOST_C::vector<int> > &u_map, int num_unitig){	
+	//set of seen pair - done to avoid simplifying graphs and writing more edges to file than needed
 	set<pair<int,int> > edge_set;
 	FILE *f;
 	f = fopen("edges.txt","w");
 	printConsole("Number of reads: "+to_string(u_map.size()));
 	#pragma omp parallel for
 	for(int i = 0; i<u_map.size();i++){
-		BOOST_C::vector<int> temp(u_map[i].begin(),u_map[i].end());
+		BOOST_C::vector<int> temp = u_map[i];
 		#pragma omp parallel for collapse(2)
 		for(int j = 0; j < temp.size();++j){
 			for(int k = 0;  k < temp.size();++k){
+				//Only need to account for unique combinations
 				if(j-k > 0){
+					//prevent multiedges by only considering ordering of nodes
 					if(temp[j] > temp[k]){
+						//check if  edge already seen
 						auto it = edge_set.find(make_pair(temp[j],temp[k]));
 						if (it == edge_set.end()){
 							#pragma omp critical
 							{
 								fprintf(f,"%d\t%d\n",temp[j],temp[k]);
-								//outf<<temp[j]<<"\t"<<temp[k]<<endl;
 								edge_set.insert(make_pair(temp[j],temp[k]));
 							}
 						}
 					}
 					else{
+						//check if edge already seen 
 						auto it = edge_set.find(make_pair(temp[k],temp[j]));
 						if (it == edge_set.end()){
 							#pragma omp critical
 							{
 								fprintf(f,"%d\t%d\n",temp[k],temp[j]);
-								//outf<<temp[k]<<"\t"<<temp[j]<<endl;
 								edge_set.insert(make_pair(temp[k],temp[j]));
 							}
 						}
@@ -199,67 +202,12 @@ void createGraph(BOOST_C::map<int,boost::unordered_set<int> > &u_map, int num_un
 			}
 		}
 	}
+	//close file
 	fclose(f);
+	
 	//clear memory
         u_map.clear();
 	edge_set.clear();	
-	/*
-	printConsole("Begin iterations");
-	#pragma omp parallel for
-	for(int i=0;i<u_map.size();i++){
-	igraph_sparsemat_t spmat,cmat;
-        igraph_vector_int_t rid,cid;
-        igraph_vector_int_init(&rid,1);
-        igraph_vector_int_init(&cid,1);
-	igraph_sparsemat_init(&spmat,num_unitig,num_unitig,UINT_MAX);
-	igraph_real_t elem;
-		BOOST_C::vector<int> temp(u_map[i].begin(),u_map[i].end());
-		#pragma omp parallel for collapse(2)
-		for(int j = 0; j < temp.size() ; j++){
-			for(int k = 0; k < temp.size(); k++){
-				if(j-k > 0){
-					if(temp[j] > temp[k]){
-						igraph_vector_int_set(&rid,0,temp[j]);
-				                igraph_vector_int_set(&cid,0,temp[k]);
-                				#pragma omp critical
-                				{
-                        				igraph_sparsemat_compress(&spmat,&cmat);
-                        				igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-                        				++elem;
-                        				igraph_sparsemat_entry(&spmat,temp[j],temp[k],1);
-                				}
-					}
-					else{
-						igraph_vector_int_set(&rid,0,temp[k]);
-                				igraph_vector_int_set(&cid,0,temp[j]);
-                				#pragma omp critical
-                				{
-                        				igraph_sparsemat_compress(&spmat,&cmat);
-                        				igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-                        				++elem;
-                        				igraph_sparsemat_entry(&spmat,temp[k],temp[j],1);
-                				}			
-					}
-				}
-			}
-		}	
-	}
-	printConsole("End iterations");
-	//clear memory
-	u_map.clear();
-	igraph_vector_int_destroy(&rid);
-	igraph_vector_int_destroy(&cid);
-
-	igraph_t graph;
-	igraph_sparsemat_compress(&spmat,&cmat);
-	igraph_sparsemat_droptol(&cmat, 2);
-	igraph_sparsemat(&graph, &cmat,0);
-	igraph_sparsemat_destroy(&spmat);
-	igraph_sparsemat_destroy(&cmat);
-	igraph_destroy(&graph);
-	printConsole("Finished iteration");
-	*/
-
 }
 
 void processGraph(){
@@ -306,148 +254,12 @@ int main(int argc, char* argv[]){
 	int num_unitig = numUnitig();
 	
 	//prepare read to unitigs dictionary
-	BOOST_C::map<int,boost::unordered_set<int> > u_map = processDictionary(u_map1,u_map2);
+	BOOST_C::map<int,BOOST_C::vector<int> > u_map = processDictionary(u_map1,u_map2);
 	
 	//build graph
 	createGraph(u_map, num_unitig);
 	
 	//process graph
 	processGraph();	
-
-	/*	
-	igraph_t graph;
-	FILE *inpf;
-	inpf = fopen("edges.txt", "r"); 
-	igraph_read_graph_edgelist(&graph, inpf, 0, 0);
-	igraph_vector_t vec;
-	igraph_vector_init(&vec,1);
-	igraph_coreness(&graph, &vec, IGRAPH_ALL);
-	*/
-	
-	/*	
-	//graph building code   
-        igraph_sparsemat_t spmat,cmat;
-	igraph_vector_int_t rid,cid;
-        igraph_vector_int_init(&rid,1);
-        igraph_vector_int_init(&cid,1);
-	igraph_sparsemat_init(&spmat,2,2,1);
-        //igraph_sparsemat_entry(&spmat,0,0,0);
-        //igraph_sparsemat_entry(&spmat,0,1,0);
-        //igraph_sparsemat_entry(&spmat,1,0,0);
-        //igraph_sparsemat_entry(&spmat,1,1,3);
-	//igraph_vector_int_t rid,cid;
-	//igraph_vector_int_init(&rid,1);
-	//igraph_vector_int_init(&cid,1);
-	//igraph_real_t elem;
-	//igraph_vector_int_set(&rid,0,1);
-	//igraph_vector_int_set(&cid,0,1);
-	//igraph_sparsemat_compress(&spmat,&cmat);
-	//igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-	//cout<<elem<<endl;	
-	//igraph_sparsemat_entry(&spmat,1,1,elem++);		
-	//igraph_sparsemat_compress(&spmat,&cmat);
-	//igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-	//cout<<elem<<endl;	
-	igraph_real_t elem;
-	#pragma omp parallel for 
-	for(int i=0; i< 100000; ++i){
-		//igraph_sparsemat_t cmat;
-		igraph_vector_int_set(&rid,0,1);
-        	igraph_vector_int_set(&cid,0,1);
-		#pragma omp critical
-		{
-			//igraph_real_t elem;
-			igraph_sparsemat_compress(&spmat,&cmat);
-			igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-			++elem;
-			igraph_sparsemat_entry(&spmat,1,1,1);
-		}
-	}
-	//igraph_sparsemat_t cmat;
-	igraph_vector_int_set(&rid,0,1);
-	igraph_vector_int_set(&cid,0,1);
-	igraph_sparsemat_compress(&spmat,&cmat);
-	igraph_sparsemat_index(&cmat,&rid,&cid, nullptr, &elem);
-	cout<<elem<<endl;
-	*/	
-
-
-	/*
-	igraph_t graph;
-	igraph_adjlist_t adj;
-	igraph_adjlist_init_empty(&adj,4);
-	igraph_vector_int_t *p = igraph_adjlist_get(&adj,0);
-	igraph_vector_int_push_back(p,3);
-	igraph_vector_int_t *q = igraph_adjlist_get(&adj,3);
-	igraph_vector_int_push_back(q,1);
-	//igraph_free(p);
-	//cout<<igraph_vector_int_size(p)<<endl;
-	igraph_adjlist(&graph, &adj, IGRAPH_ALL, 1);	
-	igraph_integer_t vcount = igraph_vcount(&graph);
-	igraph_integer_t ecount = igraph_ecount(&graph);
-	cout<<"Number of vertices: "<<vcount<<endl;
-	cout<<"Number of edges: "<<ecount<<endl;
-	igraph_adjlist_destroy(&adj);
-	igraph_destroy(&graph);
-	*/
-	
-	/*
-	//use the igraph C library for operations
-	igraph_t graph;
-	igraph_sparsemat_t spmat;
-	igraph_sparsemat_t cmat;
-	igraph_sparsemat_init(&spmat,3,3,2);
-	igraph_sparsemat_entry(&spmat,0,0,2);
-	igraph_sparsemat_entry(&spmat,0,1,0);
-	igraph_sparsemat_entry(&spmat,1,0,0);
-	igraph_sparsemat_entry(&spmat,0,2,0);
-	igraph_sparsemat_entry(&spmat,1,1,3);
-	igraph_sparsemat_entry(&spmat,1,2,0);
-	igraph_sparsemat_entry(&spmat,2,0,3);
-	igraph_sparsemat_entry(&spmat,2,1,3);
-	igraph_sparsemat_entry(&spmat,2,2,3);
-	igraph_sparsemat_compress(&spmat,&cmat);
-	igraph_sparsemat_destroy(&spmat); 
-	igraph_sparsemat_droptol(&cmat, 2);
-	igraph_sparsemat(&graph,&cmat,0);
-	igraph_integer_t vcount = igraph_vcount(&graph);
-	igraph_integer_t ecount = igraph_ecount(&graph);
-	cout<<"Number of vertices: "<<vcount<<endl;
-	cout<<"Number of edges: "<<ecount<<endl;
-	igraph_sparsemat_destroy(&cmat);
-	igraph_destroy(&graph);
-	cout<<"No Error :)"<<endl;
-	*/
-
-	/*
-	boost::unordered_set<int> uset;
-	for(int i = 0;i<1000;i++){
-		uset.insert(i);
-	}
-
-	#pragma omp parallel for 
-	for (int j = 0; j<1000;++j){
-		itertools::combine(uset);
-		cout<<"\r"<<j;
-	}
-	cout<<endl;
-	cout<<edges.size()<<endl;
-	*/
-	/*
-	int n = 1000;
-	int r = 2;
-	BOOST_C::vector<int> res;
-	BOOST_C::vector<bool> v(n);
-    	fill(v.end() - r, v.end(), true);
-    	do {
-        	for (int i = 0; i < n; ++i) {
-            		if (v[i]) {
-                		//cout << (i + 1) << " ";
-				res.push_back(i+1);
-            		}
-        	}
-        	//cout << "\n";
-    	} while (next_permutation(v.begin(), v.end()));
-	*/
 	return 0;
 }
