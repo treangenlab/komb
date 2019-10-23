a6021a5b59c677daebfa7a35d83fc1aec9b490b8
Advait Balaji <adsgarfield95@gmail.com>
2019-10-23 10:00:04 -0500

    Added Nodes and Edges output'
    Former-commit-id: 05975497550785c79a7ce84bd4d001f9599c27ca

--- komb.cpp
+++ komb.cpp
@@ -366,6 +366,7 @@ void processGraph(string dir){
 	}	
         igraph_read_graph_edgelist(&graph, inpf, 0, 0);
         fclose(inpf);
+	string graphInfo = "The Graph has "+to_string((int)igraph_vcount(&graph))+" vertices and "+to_string((int)igraph_ecount(&graph))+" edges.";
 	runAlgorithm(graph,dir);
 }
 
