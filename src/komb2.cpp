6ab7ac79454af440f3111f227b9a70c794a3e26e
Nick Sapoval <ns58@gho.cs.rice.edu>
2023-06-23 16:57:04 -0500

    Saving local changes before pull from remote

---
+++ src/komb2.cpp
@@ -0,0 +1,114 @@
+#include <cstdlib>
+#include <iostream>
+#include <CompactedDBG.hpp>
+#include <omp.h>
+#include <igraph.h>
+#include "test.h"
+#include "graph.h"
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <tclap/CmdLine.h>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#define __version__ "1.0"
+
+int main(int argc, const char** argv)
+{
+
+    /* sync cout off */
+    std::ios_base::sync_with_stdio(false);
+
+    std::string version(__version__);
+    /* check for max number of threads in the system using omp */
+    int opt_threads = omp_get_max_threads();
+
+    /* get current time info */
+    time_t curr_time = time(0);
+    tm* mdyt = localtime(&curr_time);
+    const std::string dirname = "output_"+ std::to_string(1900 + mdyt->tm_year) +
+            std::to_string(1+ mdyt->tm_mon)+ std::to_string(mdyt->tm_mday) + "_" +
+            std::to_string(mdyt->tm_hour) + std::to_string(mdyt->tm_min)+
+            std::to_string(mdyt->tm_sec);
+
+    /* parse args */
+    TCLAP::CmdLine cmd("KOMB: Taxonomy-oblivious characterization of metagenome "
+                             "dynamics", ' ', version);
+    TCLAP::ValueArg<std::string> inputArg("i", "input",
+      "Input SAM file [Default: alingment1.sam]", true,
+      "alignment1.sam", "string");
+    TCLAP::ValueArg<std::string> input2Arg("j", "input2",
+      "Second input SAM file [Default: alignment2.sam]", true,
+      "alignment2.sam", "string");
+    TCLAP::ValueArg<int> readlenArg("l", "readlen",
+       "Read Length (can be average) [Default: 151]", false, 151,
+       "int");
+    TCLAP::ValueArg<int> threadsArg("t", "threads",
+       "Number of Threads [Default: Max]", false, opt_threads,
+       "int");
+    TCLAP::ValueArg<std::string> outputArg("o", "output",
+       "Output directory [Default: output_yyyymmdd_hhmmss]", false, dirname,
+       "string");
+    cmd.add(inputArg);
+    cmd.add(input2Arg);
+    cmd.add(readlenArg);
+    cmd.add(threadsArg);
+    cmd.add(outputArg);
+
+    auto begin = std::chrono::steady_clock::now(); // start timer
+        
+    cmd.parse(argc,argv);
+    const std::string outdir = outputArg.getValue();
+    const std::string input = inputArg.getValue();
+    const std::string input2 = input2Arg.getValue();
+    const int readlen = readlenArg.getValue();
+    int threads = threadsArg.getValue();
+
+    const bool isBifrost = false;
+
+    /* create output dir */
+    const std::string create_dir = "mkdir " + outdir;
+    int return_val = std::system(create_dir.c_str());
+
+    /* run KOMB core */
+    auto kg = komb::Kgraph(threads, readlen); //use this instantiation
+    umapset umap1, umap2;
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            //#pragma omp task
+            kg.readSAM(input, umap1);
+            //#pragma omp task
+            kg.readSAM(input2, umap2);
+        }
+
+    }
+
+    fprintf(stdout, "Read SAM files\n");
+    
+    auto begin_komb = std::chrono::steady_clock::now();
+    vvec edgeinfo = kg.getEdgeInfo(umap1, umap2);
+    fprintf(stdout, "Processed edgeinfo\n");
+    kg.generateGraph(edgeinfo, outdir);
+    fprintf(stdout, "Created edgelist\n");
+    kg.readEdgeList(outdir);
+    fprintf(stdout, "Created Kcore\n");
+    kg.combineFile(outdir, isBifrost);
+    //int weight = 10; /* fix weight as 10 for now */
+    kg.anomalyDetection(outdir, true);
+    fprintf(stdout,"Identified anomalous unitigs\n");
+    kg.splitAnomalousUnitigs(outdir, isBifrost);
+    fprintf(stdout,"Created anomalouss unitigs file\n");
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now() - begin_komb).count() / 1000000.0;
+    fprintf(stdout, "\nTime elapsed for KOMB: %.3f ms\n", duration);
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now() - begin).count() / 1000000.0;
+    fprintf(stdout, "\nTime elapsed for analysis (sec) = %.2f \n", duration);
+        
+    return 0;
+
+} // main
